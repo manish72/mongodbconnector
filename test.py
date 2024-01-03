c4d25f947180f9ccf4712fb8720e7c3555ebafc9
manish72 <manishguru72@gmail.com>
2024-01-03 13:05:23 +0530

    removed old package

---
+++ test.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requiremet(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","") for req in requirements  if not req.startswith("#")]
+        requirements = [req for req in requirements if len(req)>0]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+print(get_requiremet("./requirements_dev.txt"))
